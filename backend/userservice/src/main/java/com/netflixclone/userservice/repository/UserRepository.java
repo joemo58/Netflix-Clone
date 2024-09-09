30b322399320d00ea3b964f15254ebe824da4cfc
Joe McGirr <joe.mcgirr@bjss.com>
2024-09-09 10:35:43 +1000

    Initial commit - will remove repo, entity and service layer after to get a PoC working

--- backend/userservice/src/main/java/com/netflixclone/userservice/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.netflixclone.userservice.repository;
-
-import com.netflixclone.userservice.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
